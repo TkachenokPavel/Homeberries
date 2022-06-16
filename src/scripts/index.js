806609dd277f6bc345c261ae80f58a1176a7be65
IgorAlshuk <igordilar@mail.ru>
2022-06-16 23:49:06 +0300

    refactor(hom-4): refact init function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 import { nextSlide, prevSlide } from './components/slider.js';
 
 const init = () => {
-    document.getElementById('btn-prev').addEventListener('click', nextSlide);
-    document.getElementById('btn-next').addEventListener('click', prevSlide);
+    document.getElementById('btn-prev').addEventListener('click', prevSlide);
+    document.getElementById('btn-next').addEventListener('click', nextSlide);
 
     setInterval(nextSlide, 4000);
 };
